e5bda346fac3320c70b7361888d5021d0826319e
dmytroKarataiev <karataev.dima@gmail.com>
2016-06-09 20:32:10 +0300

    feat: new exercises, docs update

---
+++ learning/dataframe_olympics.py
@@ -0,0 +1,179 @@
+from pandas import DataFrame, Series
+import numpy
+
+
+#################
+# Syntax Reminder:
+#
+# The following code would create a two-column pandas DataFrame
+# named df with columns labeled 'name' and 'age':
+#
+# people = ['Sarah', 'Mike', 'Chrisna']
+# ages  =  [28, 32, 25]
+# df = DataFrame({'name' : Series(people),
+#                 'age'  : Series(ages)})
+
+def create_dataframe():
+    '''
+    Create a pandas dataframe called 'olympic_medal_counts_df' containing
+    the data from the table of 2014 Sochi winter olympics medal counts.
+
+    The columns for this dataframe should be called
+    'country_name', 'gold', 'silver', and 'bronze'.
+
+    There is no need to  specify row indexes for this dataframe
+    (in this case, the rows will automatically be assigned numbered indexes).
+
+    You do not need to call the function in your code when running it in the
+    browser - the grader will do that automatically when you submit or test it.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    olympic_medal_counts_df = DataFrame({'country_name': Series(countries),
+                                         'gold': Series(gold),
+                                         'silver': Series(silver),
+                                         'bronze': Series(bronze)})
+
+    return olympic_medal_counts_df
+
+
+def avg_medal_count():
+    '''
+    Compute the average number of bronze medals earned by countries who
+    earned at least one gold medal.
+
+    Save this to a variable named avg_bronze_at_least_one_gold. You do not
+    need to call the function in your code when running it in the browser -
+    the grader will do that automatically when you submit or test it.
+
+    HINT-1:
+    You can retrieve all of the values of a Pandas column from a
+    data frame, "df", as follows:
+    df['column_name']
+
+    HINT-2:
+    The numpy.mean function can accept as an argument a single
+    Pandas column.
+
+    For example, numpy.mean(df["col_name"]) would return the
+    mean of the values located in "col_name" of a dataframe df.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    olympic_medal_counts = {'country_name': Series(countries),
+                            'gold': Series(gold),
+                            'silver': Series(silver),
+                            'bronze': Series(bronze)}
+    df = DataFrame(olympic_medal_counts)
+
+    avg_bronze_at_least_one_gold = numpy.mean(df[df['gold'] >= 1]['bronze'])
+
+    return avg_bronze_at_least_one_gold
+
+
+def avg_medal():
+    '''
+    Using the dataframe's apply method, create a new Series called
+    avg_medal_count that indicates the average number of gold, silver,
+    and bronze medals earned amongst countries who earned at
+    least one medal of any kind at the 2014 Sochi olympics.  Note that
+    the countries list already only includes countries that have earned
+    at least one medal. No additional filtering is necessary.
+
+    You do not need to call the function in your code when running it in the
+    browser - the grader will do that automatically when you submit or test it.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    olympic_medal_counts = {'country_name': countries,
+                            'gold': Series(gold),
+                            'silver': Series(silver),
+                            'bronze': Series(bronze)}
+    df = DataFrame(olympic_medal_counts)
+
+    avgs = df[['gold', 'silver', 'bronze']].apply(numpy.mean)
+
+    return avgs
+
+print avg_medal()
+
+def numpy_dot():
+    '''
+    Imagine a point system in which each country is awarded 4 points for each
+    gold medal,  2 points for each silver medal, and one point for each
+    bronze medal.
+
+    Using the numpy.dot function, create a new dataframe called
+    'olympic_points_df' that includes:
+        a) a column called 'country_name' with the country name
+        b) a column called 'points' with the total number of points the country
+           earned at the Sochi olympics.
+
+    You do not need to call the function in your code when running it in the
+    browser - the grader will do that automatically when you submit or test it.
+    '''
+
+    countries = ['Russian Fed.', 'Norway', 'Canada', 'United States',
+                 'Netherlands', 'Germany', 'Switzerland', 'Belarus',
+                 'Austria', 'France', 'Poland', 'China', 'Korea',
+                 'Sweden', 'Czech Republic', 'Slovenia', 'Japan',
+                 'Finland', 'Great Britain', 'Ukraine', 'Slovakia',
+                 'Italy', 'Latvia', 'Australia', 'Croatia', 'Kazakhstan']
+
+    gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
+    bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
+
+    points = numpy.dot(gold, 4) + numpy.dot(silver, 2) + numpy.dot(bronze, 1)
+
+    counts = {'country_name' : countries,
+              'points' : Series(points)}
+
+    df = DataFrame(counts)
+
+    # another alternative
+    # olympic_medal_counts = {'country_name': countries,
+    #                         'gold': Series(gold),
+    #                         'silver': Series(silver),
+    #                         'bronze': Series(bronze)}
+    # df = DataFrame(olympic_medal_counts)
+    # medal_counts = olympic_medal_counts[['gold', 'silver', 'bronze']]
+    # points = numpy.dot(medal_counts, [4, 2, 1])
+
+    # alternative way with pandas only
+    # df['points'] = df[['gold','silver','bronze']].dot([4, 2, 1])
+    # olympic_points_df = df[['country_name','points']]
+
+    return df
+
+print numpy_dot()
